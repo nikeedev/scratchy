228e4874ccfd2f4236df09601cb0098e92dded54
nikeedev <myftr12394@gmail.com>
2023-03-21 19:10:54 +0100

    updates

---
+++ README.md
@@ -0,0 +1,24 @@
+# Scratch user info
+
+Using [Scratch API](https://api.scratch.mit.edu) for getting information about a user or a project
+
+Also using Scratch's session server, you can login using a token (that will be retrieved trough the session server, to get more info like if you're a new Scratcher, or if you are banned (but are you...?)
+
+# How does it work that you get a token for session information?
+
+Well, it all begins with that Scratch has an authentication server on `https://scratch.mit.edu/login`. It's mostly recognizable as the Scratch's login page, but using HTTP, you can send an POST request in almost any programming language. You can send it your Scratch username and password in an struct, object, JSON, or whatever it is used on your language.
+
+Sending that information about you, the login server will return a token as a cookie. That token is long, but it's functionality is to describe Scratch, that **that** token identifies you as *you (username)*. It's very important that you don't show it to anybody, or else they own a quarter of your account, and they can see e.t. your email.
+
+<!-- lying on `https://scratch.mit.edu/session`. You can -->
+
+
+Here is a image preview or a sample of the program:
+![Scratch_user program in use; sample; preview](imgs/progress.png)
+
+# To-Do:
+
+- [ ] Update the headers for about me and working on.
+- [ ] Add country and time the user joined.
+- [ ] Create also new UI for user session information.
+
--- src/main.v
+++ src/main.v
@@ -1,77 +1,74 @@
 module main
+
 import net.http
-// import term
-// import time
+import term
+import time
 import json
 import os
 import zztkm.vdotenv
 
-
 /// For Authentication
 struct User {
 mut:
-	username  string
-	password  string
+	username string
+	password string
 }
+
 ///
 
 struct SessionInfo {
 	user struct {
-    	id int
-    	banned bool
-    	username string
-    	token string
-    	thumbnailUrl string
-    	dateJoined string
-    	email string
-  	}
-  	permissions struct {
-		admin bool
-		scratcher bool
-		new_scratcher bool
-		invited_scratcher bool
-		social bool
-		educator bool
-		educator_invitee bool
-		student bool
+		id           int
+		banned       bool
+		username     string
+		token        string
+		thumbnailUrl string
+		dateJoined   string
+		email        string
 	}
+
+	permissions struct {
+		admin             bool
+		scratcher         bool
+		new_scratcher     bool
+		invited_scratcher bool
+		social            bool
+		educator          bool
+		educator_invitee  bool
+		student           bool
+	}
+
 	flags struct {
-		must_reset_password bool
-		must_complete_registration bool
+		must_reset_password                bool
+		must_complete_registration         bool
 		has_outstanding_email_confirmation bool
-		show_welcome bool
-		confirm_email_banner bool
-		unsupported_browser_banner bool
-		project_comments_enabled bool
-		gallery_comments_enabled bool
-		userprofile_comments_enabled bool
-		everything_is_totally_normal bool
+		show_welcome                       bool
+		confirm_email_banner               bool
+		unsupported_browser_banner         bool
+		project_comments_enabled           bool
+		gallery_comments_enabled           bool
+		userprofile_comments_enabled       bool
+		everything_is_totally_normal       bool
 	}
 }
 
 struct ApiInfo {
-    id int
-    username string
-    scratchteam bool
-    history struct {
-        joined string
-    }
-    profile struct {
-        id int
-        status string
-        bio string
-        country string
-    }
+	id          int
+	username    string
+	scratchteam bool
+	history     struct {
+		joined string
+	}
+
+	profile struct {
+		id      int
+		status  string
+		bio     string
+		country string
+	}
 }
 
-fn main() {
-	vdotenv.load()
-	getusername := os.input("Write your Scratch username: ")
-	getpassword := os.input_password("Write your user's password: ")!
-
-	// user := User{username: os.getenv("USERNAMEenv"), password: os.getenv("PASSWORDenv")}
-	user := User{username: getusername, password: getpassword}
-
+fn print_session(user User) ! {
 	mut login_conf := http.FetchConfig{
 		url: 'https://scratch.mit.edu/login/'
 		data: json.encode(user)
@@ -80,54 +77,91 @@ fn main() {
 
 	login_conf.cookies['scratchcsrftoken'] = 'a'
 
-	login_conf.header.add_custom('X-Requested-With', 'XMLHttpRequest') !
-    login_conf.header.add_custom('X-CSRFToken', 'a') !
-	login_conf.header.add_custom('Referer', 'https://scratch.mit.edu') !
-	login_conf.header.add_custom('Cookie', 'scratchcsrftoken=a;') !
-	login_conf.header.add_custom('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36') !
-	login_conf.header.add_custom('Content-Type', 'application/json') !
+	login_conf.header.add_custom('X-Requested-With', 'XMLHttpRequest')!
+	login_conf.header.add_custom('X-CSRFToken', 'a')!
+	login_conf.header.add_custom('Referer', 'https://scratch.mit.edu')!
+	login_conf.header.add_custom('Cookie', 'scratchcsrftoken=a;')!
+	login_conf.header.add_custom('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')!
+	login_conf.header.add_custom('Content-Type', 'application/json')!
 
-	mut sessionid_response := http.fetch(login_conf) !
+	mut sessionid_response := http.fetch(login_conf)!
 	// println(sessionid_response)
-	os.write_file("login_response.json", sessionid_response.body) !
+	os.write_file('login_response.json', sessionid_response.body)!
 	my_cookie := sessionid_response.cookies()[0].value
 	// println(my_cookie)
 
-
 	mut status_conf := http.FetchConfig{
-		url: "https://scratch.mit.edu/session/",
-		data: '',
+		url: 'https://scratch.mit.edu/session/'
+		data: ''
 		method: .get
 	}
 
-	status_conf.header.add_custom('cookie', 'scratchsessionsid=${my_cookie};') !
-	status_conf.header.add_custom('X-Requested-With', 'XMLHttpRequest') !
-	status_conf.header.add_custom('Referer', 'https://scratch.mit.edu/session/') !
-	status_conf.header.add_custom('Content-Type', 'application/json') !
+	status_conf.header.add_custom('cookie', 'scratchsessionsid=${my_cookie};')!
+	status_conf.header.add_custom('X-Requested-With', 'XMLHttpRequest')!
+	status_conf.header.add_custom('Referer', 'https://scratch.mit.edu/session/')!
+	status_conf.header.add_custom('Content-Type', 'application/json')!
 
-
-	mut session_response := http.fetch(status_conf) !
+	mut session_response := http.fetch(status_conf)!
 	// println(session_response.body)
-	os.write_file("session_response.json", session_response.body) !
-	session := json.decode(SessionInfo, session_response.body) !
+	os.write_file('session_response.json', session_response.body)!
+	session := json.decode(SessionInfo, session_response.body)!
 	println(session)
+}
 
-	println("\n")
+fn (info ApiInfo) beautify() ! {
+	println(term.bright_green('User: ' + info.username + '\n'))
+	println(term.bright_blue('ID: ' + info.profile.id.str()))
+	println(term.bold('Scratchteam: ' +
+		if info.scratchteam { term.green('true') } else { term.red('false') } + ' (obviously)'))
+	// println(term.bright_white('Joined: ' + time.parse_iso8601(info.history.joined)!.format()))
+	println('\n')
+	println(term.bold('About ' + info.username))
+	println('\n' +term.header(term.bold('"About me":'), '=') + '\n')
+	println(info.profile.bio)
+	println('\n' + term.header("", "=") + '\n')
+	println('\n\n' +term.header(term.bold('"Working on":'), '=') + '\n')
+	println(info.profile.status)
+	println('\n' + term.header("", "=") + '\n')
+}
+
+fn main() {
+	wants_session := os.input('Want to verify to show session data? (y/N) ').trim_space()
+
+	vdotenv.load()
+	getusername := os.input('Write your Scratch username: ')
+
+	mut user := User{}
+	if wants_session == 'y' || wants_session == 'Y' {
+		getpassword := os.input_password("Write your user's password: ")!
+
+		user = User{
+			username: getusername
+			password: getpassword
+		}
+		// user := User{username: os.getenv("USERNAMEenv"), password: os.getenv("PASSWORDenv")}
+
+		print_session(user)!
+	} else {
+		user = User{
+			username: getusername
+			password: ''
+		}
+	}
+
+	println('\n')
 	mut api_conf := http.FetchConfig{
-		url: "https://api.scratch.mit.edu/users/${user.username}",
-		data: '',
+		url: 'https://api.scratch.mit.edu/users/${user.username}'
+		data: ''
 		method: .get
 	}
 
-	api_conf.header.add_custom('cookie', 'scratchsessionsid=${my_cookie};') !
-	api_conf.header.add_custom('X-Requested-With', 'XMLHttpRequest') !
-	api_conf.header.add_custom('Referer', 'https://api.scratch.mit.edu/') !
-	api_conf.header.add_custom('Content-Type', 'application/json') !
+	api_conf.header.add_custom('X-Requested-With', 'XMLHttpRequest')!
+	api_conf.header.add_custom('Referer', 'https://api.scratch.mit.edu/')!
+	api_conf.header.add_custom('Content-Type', 'application/json')!
 
-	mut api_response := http.fetch(api_conf) !
-	//println(api_response.body)
-	os.write_file("api_response.json", api_response.body) !
-	apis := json.decode(ApiInfo, api_response.body) !
-	println(apis)
-	println("\n\n")
+	mut api_response := http.fetch(api_conf)!
+	// println(api_response.body)
+	os.write_file('api_response.json', api_response.body)!
+	apis := json.decode(ApiInfo, api_response.body)!
+	apis.beautify()!
 }

54680b5a4823177c6d1e84b422edbc35af012c84
nikeedev <myftr12394@gmail.com>
2023-03-21 11:10:24 +0100

    updates

--- src/main.v
+++ src/main.v
@@ -6,10 +6,6 @@ import json
 import os
 import zztkm.vdotenv
 
-struct MyCookie {
-mut:
-	cookie http.Cookie
-}
 
 /// For Authentication
 struct User {
@@ -55,9 +51,12 @@ struct SessionInfo {
 
 fn main() {
 	vdotenv.load()
+	// getusername := os.input("Write your Scratch username: ")
+	// getpassword := os.input_password("Write your user's password: ")!
 
-	mut my_cookie := MyCookie{}
 	user := User{username: os.getenv("USERNAMEenv"), password: os.getenv("PASSWORDenv")}
+	// user := User{username: getusername, password: getpassword}
+	// println(user)
 
 	mut login_conf := http.FetchConfig{
 		url: 'https://scratch.mit.edu/login/'
@@ -70,14 +69,14 @@ fn main() {
 	login_conf.header.add_custom('X-Requested-With', 'XMLHttpRequest') !
     login_conf.header.add_custom('X-CSRFToken', 'a') !
 	login_conf.header.add_custom('Referer', 'https://scratch.mit.edu') !
-	// login_conf.header.add_custom('Cookie', 'scratchcsrftoken=a;') !
+	login_conf.header.add_custom('Cookie', 'scratchcsrftoken=a;') !
 	login_conf.header.add_custom('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36') !
 	login_conf.header.add_custom('Content-Type', 'application/json') !
 
 	mut sessionid_response := http.fetch(login_conf) !
 	// println(sessionid_response)
 	os.write_file("reponse.log", sessionid_response.body) !
-	my_cookie.cookie = sessionid_response.cookies()[0]
+	my_cookie := sessionid_response.cookies()[0]
 	// println(my_cookie.cookie.value)
 
 
@@ -87,11 +86,12 @@ fn main() {
 		method: .get
 	}
 
-	status_conf.header.add_custom('cookie', 'scratchsessionsid=${my_cookie.cookie.value};') !
+	status_conf.header.add_custom('cookie', 'scratchsessionsid=${my_cookie};') !
 	status_conf.header.add_custom('X-Requested-With', 'XMLHttpRequest') !
 	status_conf.header.add_custom('Referer', 'https://scratch.mit.edu/session/') !
 
 	mut session_response := http.fetch(status_conf) !
+	println(session_response.body)
 	session := json.decode(SessionInfo, session_response.body) !
 	println(session)
 }

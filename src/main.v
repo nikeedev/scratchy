f718e63c6f7b31dd80f4fc568608b316b65ea9a1
nikeedev <myftr12394@gmail.com>
2023-03-23 10:59:01 +0100

    updates

--- src/main.v
+++ src/main.v
@@ -113,7 +113,7 @@ fn (info ApiInfo) beautify() ! {
 	println(term.bright_blue('ID: ' + info.profile.id.str()))
 	println(term.bold('Scratchteam: ' +
 		if info.scratchteam { term.green('true') } else { term.red('false') } + ' (obviously)'))
-	// println(term.bright_white('Joined: ' + time.parse_iso8601(info.history.joined)!.format()))
+	println(term.bright_white('Joined: ' + time.parse_iso8601(info.history.joined)!.relative()))
 	println(term.bright_magenta('Country: ${info.profile.country}'))
 	println('\n')
 	println(term.bold('About ' + info.username))
